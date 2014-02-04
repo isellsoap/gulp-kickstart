b33df7061156a42a43d10956fda8d6f0ca395fa3
Francesco Schwarz <mail@frncs.co>
2014-02-04 18:12:49 +0100

    Add basic folder and file structure for assets

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  // var slider = $('.btk').bxSlider({
+  //   controls: false,
+  //   buildPager: function(slideIndex) {
+  //     return '<span class="bxslider-slide-index">' + slideIndex + '</span>';
+  //   },
+  //   onSlideBefore: function($slideElement, oldIndex, newIndex) {
+  //     console.log('(onSlideBefore) oldIndex: ' + oldIndex + ' newIndex: ' + newIndex);
+  //   },
+  //   onSlideAfter: function($slideElement, oldIndex, newIndex) {
+  //     console.log('(onSlideAfter) oldIndex: ' + oldIndex + ' newIndex: ' + newIndex);
+  //   }
+  // });
+
+  // $('.btk').on('click', function() {
+  //   slider.goToNextSlide();
+  //   console.log('bla');
+  // });
+  console.log('bla4');
+
+});
